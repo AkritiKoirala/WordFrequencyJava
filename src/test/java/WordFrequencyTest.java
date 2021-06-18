6a50217bedcaa7986a0e606f0981d2f64869e630
Akriti Koirala <akritikoirala01@gmail.com>
2021-06-18 23:58:57 +0545

    word frequency program competed

---
+++ src/test/java/WordFrequencyTest.java
@@ -0,0 +1,23 @@
+import junit.framework.*;
+
+public class WordFrequencyTest extends TestCase {
+    protected int value1, value2;
+
+    // assigning the values
+    protected void setUp(){
+        //arrange
+
+        //act
+
+        //assert
+    }
+
+    // test method to add two values
+    public void testIsCountTrue(){
+       //arrange
+
+        //act
+
+        //assert
+    }
+}
